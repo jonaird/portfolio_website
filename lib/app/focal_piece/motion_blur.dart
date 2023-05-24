d1a2d2b2e2e69b96938957adf89422b724b7dd9a
jonaird <jonathan.aird@gmail.com>
2023-05-24 11:06:01 +0200

    latest working shader

--- lib/app/focal_piece/motion_blur.dart
+++ lib/app/focal_piece/motion_blur.dart
@@ -14,61 +14,40 @@ class _MotionBlurState extends State<MotionBlur> {
   Size? prevSize;
   Offset? prevPosition;
   ui.Image? prevFrame;
-  late RenderBox myRenderObject;
+  late RenderBox childRenderObject;
 
   void _setRenderObject(RenderBox box) {
-    // myRenderObject = box;
+    childRenderObject = box;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ShaderBuilder((context, shader, child) {
-      final animating = context
-          .select<FocalPieceViewModel, bool>((vm) => vm.animating.value)!;
-      return AnimatedSampler(enabled: animating, (image, size, canvas) {
-        final prevFrames = context.read<FocalPieceViewModel>()!.previousFrames;
-        final prevFrame0 = prevFrames.isEmpty ? image : prevFrames.first;
-        final prevFrame1 = prevFrames.length > 1 ? prevFrames[1] : image;
-        // shader.setFloat(0, value);
-        // shader.setFloat(1, value);
-        shader.setFloat(0, size.width);
-        shader.setFloat(1, size.height);
-        shader.setImageSampler(0, image);
-        // shader.setImageSampler(1, prevFrame0);
-        // shader.setImageSampler(2, prevFrame1);
-
-        canvas.drawRect(
-          Offset.zero & size,
-          Paint()..shader = shader,
-        );
-      }, child: widget.child);
-    }, assetKey: 'shaders/motion_blur.glsl');
     // return widget.child;
     return ShaderBuilder((context, shader, child) {
       final animating = context
           .select<FocalPieceViewModel, bool>((vm) => vm.animating.value)!;
       return AnimatedSampler(
         enabled: animating,
-        (image, size, canvas) {
-          // final position = myRenderObject.localToGlobal(Offset.zero);
-          const position = Offset.zero;
+        (frame, size, canvas) {
+          final position = childRenderObject.localToGlobal(Offset.zero);
+          // const position = Offset.zero;
           shader
             ..setFloat(0, size.width)
             ..setFloat(1, size.height)
-            // ..setFloat(2, position.dx)
-            // ..setFloat(3, position.dy)
-            // ..setFloat(4, (prevSize ?? size).width)
-            // ..setFloat(5, (prevSize ?? size).height)
-            // ..setFloat(6, (prevPosition ?? position).dx)
-            // ..setFloat(7, (prevPosition ?? position).dy)
-            ..setImageSampler(0, image);
+            ..setFloat(2, position.dx)
+            ..setFloat(3, position.dy)
+            ..setFloat(4, (prevSize ?? size).width)
+            ..setFloat(5, (prevSize ?? size).height)
+            ..setFloat(6, (prevPosition ?? position).dx)
+            ..setFloat(7, (prevPosition ?? position).dy)
+            ..setImageSampler(0, frame);
           // ..setImageSampler(1, prevFrame ?? image);
           canvas.drawRect(
             Offset.zero & size,
             Paint()..shader = shader,
           );
           prevSize = size;
-          prevFrame = image;
+          prevFrame = frame;
           prevPosition = position;
         },
         child: MotionBlurRenderObjectWidget(
--- shaders/motion_blur.glsl
+++ shaders/motion_blur.glsl
@@ -5,11 +5,11 @@ precision highp float;
 #include <flutter/runtime_effect.glsl>
 
 
-layout(location = 0) uniform vec2 uSize;
-// layout(location = 1) uniform vec2 uPosition;
-// layout(location = 2) uniform vec2 uPrevSize;
-// layout(location = 3) uniform vec2 uPrevPosition;
-layout(location = 1) uniform sampler2D uFrame;
+layout(location = 0) uniform vec2 size;
+layout(location = 1) uniform vec2 position;
+layout(location = 2) uniform vec2 prevSize;
+layout(location = 3) uniform vec2 prevPosition;
+layout(location = 4) uniform sampler2D frame;
 // layout(location = 5) uniform sampler2D prevFrame0;
 //layout(location = 3) uniform sampler2D prevFrame1;
 
@@ -20,14 +20,14 @@ void main() {
  // vec2 puv = round(uv * uPixels) / uPixels;
   //fragColor = texture(uTexture, puv);
   //fragColor = vec4(1.0);
-  vec2 uv = FlutterFragCoord().xy/uSize;
+  vec2 uv = FlutterFragCoord().xy/size;
  // vec4 frame = texture(uTexture,uv);
   //vec4 prevFrame0 = texture(prevFrame0,uv);
   //vec4 prevFrame1 = texture(prevFrame1,uv);
   const int n = 50;
   vec4 pixel = vec4(0);
   for(int i=0;i<n;i++){
-    pixel+=texture(uFrame, uv + vec2(float(i)));
+    pixel+=texture(frame, uv + vec2(float(i)));
   }
   pixel/=n;
   fragColor = pixel;
