094be0b5ed94a140f9d544744ddb041741f7df94
jonaird <jonathan.aird@gmail.com>
2023-06-28 10:58:58 +0200

    Built web
    Branch: main
    Commit: fb4f3b87957fd2ca4b861d9b37042b1333bd2be6
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0ade9fd00441687d64dd4a951d7c7399",
-"index.html": "69d082f156af1278aef291b3b7c3c29c",
-"/": "69d082f156af1278aef291b3b7c3c29c",
-"main.dart.js": "8fbfe3ef23552a7b26ed86c76838b397",
+"index.html": "08a02ef5034fc77fcfd67f87d76c5be5",
+"/": "08a02ef5034fc77fcfd67f87d76c5be5",
+"main.dart.js": "8d05129171a3050e440425e024aa11d7",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
